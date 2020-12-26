9b1773cc3650b67d2926b1e6f24f75122324185d
Josh Hansen <hansen.joshuaa@gmail.com>
2020-12-26 15:44:30 -0800

    Demonstrate a problem with typechecking

--- src/Test.vue
+++ src/Test.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
-    {{ fo }}
+    {{ bar }}
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import * as _ from 'lodash'
+import { defineComponent } from 'vue';
 
-export default {
-  props: ['foo', 'bar']
+interface Data {
+  x: number;
 }
+
+export default defineComponent({
+  props: ['foo', 'bar'],
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  mounted() {
+    // This does not give an error because `this.count` is seen as having type `any`
+    const result = this.count.split('') // => Property 'split' does not exist on type 'number'
+
+    // This does give an error, indicating that typechecking is working at least somewhat
+    const n: number = 'abc';
+  }
+});
 </script>
